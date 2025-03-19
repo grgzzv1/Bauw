5522b1fcbc278c0cb63094e4d790f436970630be
Samel Grgzz <grgxpemba3@gmail.com>
2025-03-19 18:10:31 +0545

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,10 @@
 # run.py
 
 try:
-    with open("Tetsu.txt", "r") as file:
+    with open("Tetsu.py", "r") as file:
         script_content = file.read().strip()  # Remove extra spaces
-        exec(script_content)  # Execute the script inside Tetsu.txt
+        exec(script_content)  # Execute the script inside Tetsu.py
 except FileNotFoundError:
-    print("Error: Tetsu.txt not found! Make sure it is in the same directory.")
+    print("Error: Tetsu.py not found! Make sure it is in the same directory.")
 except Exception as e:
     print(f"An error occurred: {e}")
