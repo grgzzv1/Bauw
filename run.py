2c2ab99909e10b5ced1874e4d333b4c5e664e87e
Samel Grgzz <grgxpemba3@gmail.com>
2025-03-19 17:45:28 +0545

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,9 @@
 
 # Open and read the contents of Tetsu.txt
 try:
-    with open("Tetsu.txt", "r") as file:
+    with open("Tetsu.so", "r") as file:
         content = file.read()
-        print("Contents of Tetsu.txt:")
+        print("Contents of Tetsu.so:")
         print(content)
 except FileNotFoundError:
     print("Error: Tetsu.txt not found! Make sure it is in the same directory.")
